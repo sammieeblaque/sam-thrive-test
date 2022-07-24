dd44579a97ef0d5b584427d9458c6fe0fd0fc7d5
Samuel Airehrour <samuel@wellnite.com>
2022-07-24 12:12:25 +0100

    chore: displayed the organisations and repo

--- src/components/SingleUser/index.tsx
+++ src/components/SingleUser/index.tsx
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 const SingleUser = (): JSX.Element => {
-  const [userInfo, setuserInfo] = useState<any>([]);
+  const [userInfo, setuserInfo] = useState<any[]>([]);
   const [userOrg, setUserOrg] = useState<any[]>([]);
   const { login } = useParams();
   const { GET_USER } = config;
 
-  const fetchGituser = async () => {
+  const fetchGituserRepos = async () => {
     try {
-      const res = await fetch(`${GET_USER}${login}`);
+      const res = await fetch(`${GET_USER}${login}/repos`);
       const data = await res.json();
       console.log(data);
       setuserInfo(data);
@@ -29,19 +29,19 @@ const SingleUser = (): JSX.Element => {
     }
   };
   useEffect(() => {
-    fetchGituser();
+    fetchGituserRepos();
     fetchGitUserOrgs();
   }, []);
 
-  const {
-    name,
-    avatar_url,
-    twitter_username,
-    followers,
-    public_repos,
-    // company,
-    blog,
-  } = userInfo;
+  // const {
+  //   name,
+  //   avatar_url,
+  //   twitter_username,
+  //   followers,
+  //   public_repos,
+  //   // company,
+  //   blog,
+  // } = userInfo;
   return (
     <>
       <section className="p-5">
@@ -52,23 +52,31 @@ const SingleUser = (): JSX.Element => {
         </Link>
       </section>
       <section className="flex justify-center mt-16">
-        <section className="bg-base-400 w-2/4 mt-12 rounded-md shadow-xl px-3 py-8 flex justify-around items-center">
+        <section className="bg-base-400 w-3/4 mt-12 rounded-md shadow-xl px-3 py-8 flex justify-around items-center">
           <section className="w-2/5">
-            <img
-              src={avatar_url}
-              alt="avatar"
-              className="rounded-xl border border-neutral-700"
-            />
+            <h3 className="font-bold">Organisations</h3>
+            {userOrg.length > 0
+              ? userOrg.map((user) => (
+                  <section key={user.id} className="font-semibold p-2">
+                    <p>Name: {user.login ?? "No User Login"}</p>
+                    <p>
+                      Description: {user.description ?? "No User Description"}
+                    </p>
+                  </section>
+                ))
+              : "No Organisations Associated with user"}
           </section>
           <section>
-            <section className="font-semibold">
-              <h3>Name: {name ?? "N/A"}</h3>
-              <p>Twitter: {twitter_username ?? "N/A"}</p>
-              <p>followers: {followers ?? "N/A"} </p>
-              <p>
-                <a href={blog}>{blog ?? "N/A"}</a>
-              </p>
-              <p>repos:{public_repos ?? "N/A"}</p>
+            <section className="w-5/5">
+              <h3 className="font-bold">Repos</h3>
+              {userInfo.length > 0
+                ? userInfo.slice(0, 5).map((repo) => (
+                    <section key={repo.id} className="font-semibold p-2">
+                      <p>Name: {repo.name}</p>
+                      <p>Description: {repo.full_name}</p>
+                    </section>
+                  ))
+                : "No Repos Associated with user"}
             </section>
           </section>
         </section>
